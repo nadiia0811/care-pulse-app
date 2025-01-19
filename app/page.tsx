e1c7cbe99d095943ae4413d7e3002cdb5971d778
Nadya <nb1985@ukr.net>
2025-01-19 14:24:23 +0200

    feat: add appointment details to success appointment page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
                     <div className="text-14-regular mt-20 flex justify-between">
                         <p className="justify-items-end text-dark-600 xl:text-left">
-                          © 2024 CarePulse
+                          © 2025 CarePulse
                         </p> 
                         <Link href="?admin=true" className="text-green-500">
                           Admin
