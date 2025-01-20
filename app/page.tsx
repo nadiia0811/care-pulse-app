4fb5724084e9f1249c00fcf2bea6e3484011742b
Nadya <nb1985@ukr.net>
2025-01-20 10:27:57 +0200

    feat: add PasskeyModal component with OTP validation for admin access Use shadcn OTP component for layout and input handling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({searchParams} : SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
+ 
   return (
         <div className="flex flex-col h-screen max-h-screen md:flex-row">
 
-            {/* OTP Verification | PasskeyModal */}
+            { isAdmin && <PasskeyModal />}
+            
             <section className="remove-scrollbar my-auto container">
                 <div className="sub-container max-w-[496px]">
                     <Image src="/assets/icons/logo-full.svg"
@@ -18,10 +22,10 @@ export default function Home() {
 
                     <div className="text-14-regular mt-20 flex justify-between">
                         <p className="justify-items-end text-dark-600 xl:text-left">
-                          © 2025 CarePulse
+                            © 2025 CarePulse
                         </p> 
                         <Link href="?admin=true" className="text-green-500">
-                          Admin
+                            Admin
                         </Link>
                     </div>
                 </div>
